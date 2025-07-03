3f3ca0f4bc48a6211b1d1aa72004e31f72ff2d37
sandro <asdf>
2025-07-03 02:06:44 -0500

    simplified fetch_ohlcv

--- data_ingestion/fetch_ohlcv.py
+++ data_ingestion/fetch_ohlcv.py
@@ -2,7 +2,6 @@ import pandas as pd
 import asyncio
 import os
 import re
-import random
 
 def clean_symbol(sym, replacement='_'):
     return re.sub(r'[<>:"/\\|?*\x00-\x1F]', replacement, sym).strip()
@@ -26,21 +25,22 @@ async def fetch_symbol(exchange, symbol, timeframe='1m', limit=500, save_dir='da
         return None
 
 
-async def fetch_with_delay(exchange, symbol, timeframe, limit, save_dir, delay=1.0):
+async def fetch_with_delay(exchange, symbol, timeframe, limit, save_dir, delay):
     await asyncio.sleep(delay)
-    return await fetch_symbol(exchange, symbol, timeframe, limit, save_dir)
+    return fetch_symbol(exchange, symbol, timeframe, limit, save_dir)
 
 
 async def fetch_symbols_ohlcv(exchange, symbols, timeframe='1m', limit=500, save_dir='data'):
     batch_size = 15
     delay_between_batches = 1.0
+    delay_between_fetches = exchange.rateLimit * 1.25
 
     for i in range(0, len(symbols), batch_size):
-        symbol_batch = symbols[i:i + batch_size]
+        symbol_batch = symbols[batch_size*i:batch_size*(i+1)]
         tasks = []
         for j, symbol in enumerate(symbol_batch):
-            tasks.append(asyncio.create_task(fetch_with_delay(exchange, symbol, timeframe, limit, save_dir, delay=j*random.uniform(0.05, 0.1))))
-
+            delay = j*delay_between_fetches
+            tasks.append(fetch_with_delay(exchange, symbol, timeframe, limit, save_dir, delay))
         await asyncio.gather(*tasks)
         await asyncio.sleep(delay_between_batches)
 
