d1ab17d572c274115de8dc937b60124ebae932c9
sandro <dronic7@icloud.com>
2025-07-20 01:05:32 -0500

    cleaned up function calls, added portfolio data saving for metrics

--- main_backtest.py
+++ main_backtest.py
@@ -1,5 +1,7 @@
 import asyncio
 import ccxt.async_support as ccxt
+from datetime import datetime, timezone
+import pickle
 
 from data_ingestion.fetch_ohlcv import fetch_symbols_ohlcv
 from data_ingestion.prepare_data import prepare_candle_data
@@ -10,12 +12,11 @@ from backtest.candle_backtester.candle_executor import DummyExecutor
 from backtest.candle_backtester.candle_simulator import DummySimulator
 from backtest.candle_backtester.candle_portfolio import Portfolio
 from backtest.candle_backtester.candle_logger import Logger
-from backtest.candle_backtester.candle_metrics import Metrics
 
 #define strategy
 use_precomputed_features = True
-window = 20
-symbol = 'BTC/USDT'
+window = 40
+symbol = 'ETH/USDT'
 price_col_name = 'close'
 zscores = {
     'long_entry':-1,
@@ -33,24 +34,37 @@ strategy_params = {
 strategy = MeanReversionBasic(strategy_params)
 
 #prepare data
+year, month, day = 2022, 1, 1
+dt = datetime(year, month, day, tzinfo=timezone.utc)
+
 exchange = ccxt.mexc()
 symbols = [symbol]
-features = strategy.required_features
 timeframe = '1d'
-limit = 600
+since_ms = int(dt.timestamp() * 1000)
+limit = 1000
 path_to_csvs = 'data_ingestion/data'
-path_to_merged_data = 'data_ingestion/clean_data'
+
+fetch_params = {
+    'exchange':exchange,
+    'symbols':symbols,
+    'timeframe':timeframe,
+    'limit':limit,
+    'since':since_ms,
+    'save_dir':path_to_csvs
+}
 
 async def main():
-    try: await fetch_symbols_ohlcv(exchange, symbols, timeframe, limit, path_to_csvs)
+    try: await fetch_symbols_ohlcv(**fetch_params)
     finally: await exchange.close()
 asyncio.run(main())
 
+features = strategy.required_features
+path_to_merged_data = 'data_ingestion/clean_data'
 candle_df = prepare_candle_data(symbols, use_precomputed_features, features, path_to_csvs, path_to_merged_data)
 
 #define backtesting classes
 logger = Logger(base_path='logs/dummy_test')
-portfolio = Portfolio(logger, init_cash=1000000.0)
+portfolio = Portfolio(logger, init_cash=100000.0)
 simulator = DummySimulator()
 executor = DummyExecutor(portfolio, simulator)
 backtester = Backtester(strategy, executor, simulator, portfolio, logger, candle_df)
@@ -58,14 +72,20 @@ backtester = Backtester(strategy, executor, simulator, portfolio, logger, candle
 #run strategy
 backtester.run_backtest()
 
-#run metrics
-metrics = Metrics(portfolio)
+#save portfolio information for metrics and visualizations
+portfolio_data = {
+    'timeframe':timeframe,
+    'timestamps':portfolio.timestamps,
+    'equity_curve':portfolio.equity_curve,
+    'max_equity':portfolio.max_equity,
+    'drawdowns':portfolio.drawdowns,
+    'max_drawdown_amt':portfolio.max_drawdown_amt,
+    'max_drawdown_pct':portfolio.max_drawdown_pct,
+    'trade_history':portfolio.trade_history,
+    'total_fees':portfolio.total_fees
+}
+portfolio_name = strategy.__class__.__name__
+portfolio_path = 'metrics/portfolio_data/' + portfolio_name + '.pkl'
 
-print(metrics.compute_stats())
-print(metrics.return_basic_stats())
-print(portfolio.trade_history)
-
-import matplotlib.pyplot as plt
-
-plt.plot(metrics.equity_curve.index, metrics.equity_curve['equity'])
-plt.show()
+with open(portfolio_path, 'wb') as f:
+    pickle.dump(portfolio_data, f)
