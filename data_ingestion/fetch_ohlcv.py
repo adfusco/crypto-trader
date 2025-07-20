e886dcdd6238d3fb18d49b1045794579e4ec42b6
sandro <dronic7@icloud.com>
2025-07-20 01:03:26 -0500

    defined loop for larger limits and different start dates

--- data_ingestion/fetch_ohlcv.py
+++ data_ingestion/fetch_ohlcv.py
@@ -2,48 +2,100 @@ import pandas as pd
 import asyncio
 import os
 import re
+from datetime import datetime, timedelta
 
 def get_path_symbol(sym, replacement='_'):
     return re.sub(r'[<>:"/\\|?*\x00-\x1F]', replacement, sym).strip()
 
 
-async def fetch_symbol(exchange, symbol, timeframe='1m', limit=500, save_dir='data'):
+def get_minutes(timeframe):
+    unit = timeframe[-1]
+    unit_minutes = {
+        'm': 1,
+        'h': 60,
+        'd': 1440,
+        'w': 10080,
+        'M': 43829
+    }
+
+    return unit_minutes[unit]
+
+
+async def fetch_symbol(exchange, symbol, symbol_dataframes, timeframe='1d', limit=500, since=None, save_dir='data'):
     os.makedirs(save_dir, exist_ok=True)
     if symbol not in exchange.markets:
         raise ValueError(f"symbol {symbol} not found on {exchange.id}")
 
     try:
-        print(f'fetching {symbol}...')
-        data = await exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
+        print(f'fetching {symbol} since {since}...')
+        data = await exchange.fetch_ohlcv(symbol, timeframe, limit=limit, since=since)
         print(f"{symbol}: fetched {len(data)} rows")
 
-        candle_df = pd.DataFrame(data, columns=['timestamp', f'open', f'high', f'low', f'close', f'volume'])
-        candle_df['timestamp'] = pd.to_datetime(candle_df['timestamp'], unit='ms')
+        new_dataframe = pd.DataFrame(data, columns=['timestamp', f'open', f'high', f'low', f'close', f'volume'])
+        new_dataframe['timestamp'] = pd.to_datetime(new_dataframe['timestamp'], unit='ms')
 
-        symbol_name = get_path_symbol(symbol) + '.csv'
-        candle_df.to_csv(os.path.join(save_dir, symbol_name), index=False)
+        old_df = symbol_dataframes.get(symbol)
+        if old_df is not None:
+            new_dataframe = pd.concat([old_df, new_dataframe], ignore_index=True)
+        symbol_dataframes[symbol] = new_dataframe
+
+        last_timestamp = new_dataframe.iloc[-1]['timestamp']
+        last_timestamp_ms = int(last_timestamp.timestamp() * 1000)
+        return last_timestamp_ms
 
     except Exception as e:
         print(f'error fetching {symbol}: {e}')
         return None
 
 
-async def fetch_with_delay(exchange, symbol, timeframe, limit, save_dir, delay):
+async def fetch_with_delay(exchange, symbol, symbol_dataframes, timeframe, limit, since, save_dir, delay):
     await asyncio.sleep(delay)
-    await fetch_symbol(exchange, symbol, timeframe, limit, save_dir)
+    return await fetch_symbol(exchange, symbol, symbol_dataframes, timeframe, limit, since, save_dir)
 
 
-async def fetch_symbols_ohlcv(exchange, symbols, timeframe='1m', limit=500, save_dir='data'):
+async def fetch_symbols_ohlcv(exchange, symbols, timeframe='1d', limit=500, since=None, save_dir='data'):
     batch_size = 15
     delay_between_batches = 1.0
     delay_between_fetches = exchange.rateLimit * 1.25
 
+    symbol_dataframes = {}
+    symbol_since_timestamps = {}
+    for symbol in symbols:
+        if since is None:
+            minutes = get_minutes(timeframe)
+            since = datetime.now() - timedelta(minutes=minutes)
+        symbol_since_timestamps[symbol] = since
+
     await exchange.load_markets()
-    for i in range(0, len(symbols), batch_size):
-        symbol_batch = symbols[i:i+batch_size]
-        tasks = []
-        for j, symbol in enumerate(symbol_batch):
-            delay = j*delay_between_fetches
-            tasks.append(fetch_with_delay(exchange, symbol, timeframe, limit, save_dir, delay))
-        await asyncio.gather(*tasks)
-        await asyncio.sleep(delay_between_batches)
+
+    while limit > 0:
+        for i in range(0, len(symbols), batch_size):
+            symbol_batch = symbols[i:i+batch_size]
+            tasks = []
+
+            for j, symbol in enumerate(symbol_batch):
+                delay = j*delay_between_fetches
+                fetch_delay_params = {
+                    'exchange':exchange,
+                    'symbol':symbol,
+                    'symbol_dataframes':symbol_dataframes,
+                    'timeframe':timeframe,
+                    'limit':min(limit, 500),
+                    'since':symbol_since_timestamps[symbol],
+                    'save_dir':save_dir,
+                    'delay':delay
+                }
+                tasks.append(fetch_with_delay(**fetch_delay_params))
+
+            last_timestamps = await asyncio.gather(*tasks)
+            for symbol, last_timestamp in zip(symbol_batch, last_timestamps):
+                symbol_since_timestamps[symbol] = last_timestamp
+
+            await asyncio.sleep(delay_between_batches)
+        limit -= 500
+
+    for symbol, dataframe in symbol_dataframes.items():
+        symbol_name = get_path_symbol(symbol) + '.csv'
+        full_path = os.path.join(save_dir, symbol_name)
+
+        dataframe.to_csv(full_path, index=False)
