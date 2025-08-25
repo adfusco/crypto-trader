42e1716a7a84f02483a243887f47c9abede10033
sandro <dronic7@icloud.com>
2025-08-25 18:04:43 -0400

    added support for multiple fills in an order

--- backtest/candle_backtester/candle_simulator.py
+++ backtest/candle_backtester/candle_simulator.py
@@ -4,29 +4,35 @@ class DummySimulator:
         self.slippage_bps = slippage_bps
         self.fee_rate = fee_rate
 
-    def simulate_order(self, order, candle_row):
+    def simulate_order(self, orders, candle_row):
         ts = candle_row['timestamp']
-        symbol = order['symbol']
-        side = order['side']
-        qty = order['qty']
-        order_type = order['order_type']
+        fills = []
 
-        price_col = 'close'
-        raw_price = candle_row[f'{symbol}_{price_col}']
+        for symbol, order in orders:
+            side = order['side']
+            qty = order['qty']
+            order_type = order['order_type']
 
-        slip = (raw_price * self.slippage_bps) / 10000
-        direction = 1 if side == 'buy' else -1
-        exec_price = raw_price + (slip*direction)
+            price_col = 'close'
+            raw_price = candle_row[f'{symbol}_{price_col}']
 
-        fee = self.fee_rate * exec_price * qty
+            slip = (raw_price * self.slippage_bps) / 10000
+            direction = 1 if side == 'buy' else -1
+            exec_price = raw_price + (slip*direction)
 
-        return {
-            'timestamp':ts,
-            'symbol':symbol,
-            'side':side,
-            'qty':qty,
-            'raw_price':raw_price,
-            'execution_price':exec_price,
-            'fee':fee,
-            'order_type':order_type
-        }
+            fee = self.fee_rate * exec_price * qty
+
+            fills.append(
+                {
+                'timestamp':ts,
+                'symbol':symbol,
+                'side':side,
+                'qty':qty,
+                'raw_price':raw_price,
+                'execution_price':exec_price,
+                'fee':fee,
+                'order_type':order_type
+            }
+            )
+
+        return fills
